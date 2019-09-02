b4e17fd382a20450071645fbd52457b89556c00d
Saransh <saranshkhetarpaul99@gmail.com>
2019-09-02 22:35:04 +0530

    added dist

---
+++ dist/precache-manifest.1c3069c5a16430026ffe8364e17aa0d7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "34e2dabe46c7e55a70f5340e8d753f32",
+    "url": "/index.html"
+  },
+  {
+    "revision": "15fd1a1a1719f2dce124",
+    "url": "/static/css/2.ff9ea1f9.chunk.css"
+  },
+  {
+    "revision": "0f2e5300775c613addf0",
+    "url": "/static/css/main.382ccedb.chunk.css"
+  },
+  {
+    "revision": "15fd1a1a1719f2dce124",
+    "url": "/static/js/2.2e94e0d0.chunk.js"
+  },
+  {
+    "revision": "0f2e5300775c613addf0",
+    "url": "/static/js/main.60b59297.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
--- dist/service-worker.js
+++ dist/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.0230b2b670dad4dbe42a00574f10ba97.js"
+  "/precache-manifest.1c3069c5a16430026ffe8364e17aa0d7.js"
 );
 
 self.addEventListener('message', (event) => {
